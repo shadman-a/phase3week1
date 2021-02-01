aaca3294f96db72103da435bb842851f63b20e38
shadman-a <ahmedshadman966@gmail.com>
2021-02-01 16:04:06 -0500

    Initial commit

---
+++ src/main/java/com/example/phase3week1/Phase3week1Application.java
@@ -0,0 +1,17 @@
+package com.example.phase3week1;
+
+import com.example.phase3week1.controller.WelcomeController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Phase3week1Application {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(Phase3week1Application.class, args);
+	}
+
+}
